4975026d9010aac1c042c2576b8d2f9956912d8e
cxp <276629352@qq.com>
2019-11-05 17:30:02 +0800

    mysql脚本添加

--- src/main/resources/sql/consume_channel.sql
+++ src/main/resources/sql/consume_channel.sql
@@ -23,3 +23,28 @@ insert into consume_channel_info (channel_code,channel_name,channel_remark) VALU
 insert into consume_channel_info (channel_code,channel_name,channel_remark) VALUES ('C002','支付宝余额','支付宝余额');
 insert into consume_channel_info (channel_code,channel_name,channel_remark) VALUES ('C003','招行信用卡7298','招行信用卡7298');
 insert into consume_channel_info (channel_code,channel_name,channel_remark) VALUES ('C004','平安信用卡1273','平安信用卡1273');
+
+--==========================================================================================
+--==========================================================================================
+--mysql的sql脚本
+create table if not exists consume_channel_info(
+  id int not null AUTO_INCREMENT,
+  channel_code varchar(30) not null,
+  channel_name varchar(30) not null,
+  channel_remark varchar(255),
+  constraint consume_channel_info_primary_key_id primary key (id)
+);
+
+ALTER table consume_channel_info COMMENT  =  '消费渠道表';
+#表字段说明
+ALTER table consume_channel_info  MODIFY column id int COMMENT  '主键Id';
+ALTER table consume_channel_info  MODIFY column channel_code varchar(30) COMMENT '渠道数字代号';
+ALTER table consume_channel_info  MODIFY column channel_name varchar(30) COMMENT '渠道名称';
+ALTER table consume_channel_info  MODIFY column channel_remark varchar(255) COMMENT '渠道备注';
+#创建索引
+create index consume_channel_info_channel_code on consume_channel_info(channel_code);
+#添加基础数据
+insert into consume_channel_info (channel_code,channel_name,channel_remark) VALUES ('C001','花呗','支付宝花呗支付');
+insert into consume_channel_info (channel_code,channel_name,channel_remark) VALUES ('C002','支付宝余额','支付宝余额');
+insert into consume_channel_info (channel_code,channel_name,channel_remark) VALUES ('C003','招行信用卡7298','招行信用卡7298');
+insert into consume_channel_info (channel_code,channel_name,channel_remark) VALUES ('C004','平安信用卡1273','平安信用卡1273');
